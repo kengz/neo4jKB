538876616f58229c44009331e9f7f5d900049fc8
kengz <kengzwl@gmail.com>
2016-01-22 14:42:43 -0500

    establish test format

--- test/asset.js
+++ test/asset.js
@@ -1,12 +1,110 @@
+var _ = require('lomath')
+var Promise = require('bluebird');
+
 var cons = require('../lib/constrain')
 // Assets used in unit tests
+
+// construct a graph for testing, must have: 
+// path > 3 units
+// >1 degree
+// cycle
+// multiedge
+
+// A -> {1,2,3}
+// 1 -> 2 -> 3 -> 1
+// A -> B -> C -> D -> Z
+// D -[:test_next_2]-> Z
+
 var A = {
-  label: 'test',
+  KB: require('../index')({ NEO4J_AUTH: 'neo4j:0000' }),
+  labelNode: 'test',
   propA: cons.legalize({ name: 'A', hash_by: 'name' }),
   propB: cons.legalize({ name: 'B', hash_by: 'name' }),
   propC: cons.legalize({ name: 'C', hash_by: 'name' }),
   propD: cons.legalize({ name: 'D', hash_by: 'name' }),
-	propZ: cons.legalize({ name: 'Z', hash_by: 'name' }),
+  propZ: cons.legalize({ name: 'Z', hash_by: 'name' }),
+  prop1: cons.legalize({ name: 1, hash_by: 'name' }),
+  prop2: cons.legalize({ name: 2, hash_by: 'name' }),
+  prop3: cons.legalize({ name: 3, hash_by: 'name' }),
+  labelEdge: 'test_next',
+  labelEdge2: 'test_next_2',
+  propE: cons.legalize({ name: 'E', hash_by: 'name' }),
+
+  flush: flush,
+  buildGraph: buildGraph
 }
+
+// build the graph: first clear the test, then buildNodes, buildEdges
+function buildGraph() {
+  return new Promise(function(resolve, reject) {
+    clearTest()
+    .then(buildNodes)
+    .then(buildEdges)
+    // .then(A.KB.log)
+    .then(flush)
+    .then(resolve)
+    .catch(reject)
+  })
+}
+
+// helper function to flush the resolved args from buildGrapg
+function flush() {
+  return 
+}
+
+// clear out the test nodes
+function clearTest() {
+  return new Promise(function(resolve, reject) {
+    A.KB.query('MATCH (a:test) DETACH DELETE a')
+      .then(resolve)
+      .catch(reject)
+  })
+}
+
+// build the nodes
+function buildNodes() {
+  return new Promise(function(resolve, reject) {
+    A.KB.addNode(
+      [A.propA, A.labelNode],
+      [A.propB, A.labelNode],
+      [A.propC, A.labelNode],
+      [A.propD, A.labelNode],
+      [A.propZ, A.labelNode],
+      [A.prop1, A.labelNode],
+      [A.prop2, A.labelNode],
+      [A.prop3, A.labelNode]
+      )
+      // .then(A.KB.log)
+      .then(resolve)
+      .catch(reject)
+  })
+}
+
+// build the edges
+function buildEdges() {
+  return new Promise(function(resolve, reject) {
+    A.KB.addEdge(
+      // A -> {1,2,3}
+      [[A.propE, A.labelEdge], [A.propA], [A.prop1]],
+      [[A.propE, A.labelEdge], [A.propA], [A.prop2]],
+      [[A.propE, A.labelEdge], [A.propA], [A.prop3]],
+      // 1 -> 2 -> 3 -> 1
+      [[A.propE, A.labelEdge], [A.prop1], [A.prop2]],
+      [[A.propE, A.labelEdge], [A.prop2], [A.prop3]],
+      [[A.propE, A.labelEdge], [A.prop3], [A.prop1]],
+      // A -> B -> C -> D -> Z
+      [[A.propE, A.labelEdge], [A.propA], [A.propB]],
+      [[A.propE, A.labelEdge], [A.propB], [A.propC]],
+      [[A.propE, A.labelEdge], [A.propC], [A.propD]],
+      [[A.propE, A.labelEdge], [A.propD], [A.propZ]],
+      // D -[:test_next_2]-> Z
+      [[A.propE, A.labelEdge2], [A.propD], [A.propZ]]
+      )
+      // .then(A.KB.log)
+      .then(resolve)
+      .catch(reject)
+  })
+}
+
 
 module.exports = A;
--- test/test.js
+++ test/test.js
@@ -1,69 +1,29 @@
-// chai assertation library
-var chai = require('chai');
-var should = chai.should();
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-
-// import assets
-var A = require('./asset')
+// dependencies
 var _ = require('lomath')
-  // import test subject
+var Promise = require('bluebird');
+// chai assertation library
+var chai = require('chai')
+var chaiAsPromised = require("chai-as-promised")
+chai.use(chaiAsPromised)
+var should = chai.should()
 
-var KB = require('../index')({
-  NEO4J_AUTH: 'neo4j:0000'
-})
+// import assets and test subject
+var A = require('./asset')
+var KB = A.KB
 
-
-// tests
-// console.log(_.functions(KB))
-
-// KB.query('MATCH (n:alpha) RETURN (n)')
-//   .then(KB.log)
-//   .catch(function(err) {
-//     KB.log('error:', err)
-//   })
-
-
-// KB.getNode(prop2).then(_.flow(JSON.stringify, console.log))
-// KB.getNode('alpha').then(_.flow(JSON.stringify, console.log))
-// 
 suite('query', function() {
-  before(function() {
-    KB.query('MATCH (a:test) DETACH DELETE a')
-      // addNode(propA, 'alpha').then(_.flow(JSON.stringify, console.log))
-    KB.addNode(A.propA, 'test')
-    // add a network used to test all shits
+  before(function(done) {
+    A.buildGraph().then(done)
   })
+
   test('(query)', function() {
-    KB.query('MATCH (a:alpha) RETURN (a)').should.eventually.equal('[{"columns":["n"],"data":[]}]')
+    return KB.query('MATCH (a:test) RETURN a.name').then(KB.beautify).then(KB.string).should.eventually.equal('[{"columns":["a.name"],"data":[[1],[2],[3],["A"],["B"],["C"],["D"],["Z"]]}]')
   })
 
   test('(query, param)', function() {
-    KB.query('CREATE (a:alpha {propA}) RETURN (a)').should.eventually.equal('[{"columns":["n"],"data":[]}]')
+    return KB.query('MATCH (a:test {name: {propA}.name}) RETURN a.name', {
+      propA: A.propA
+    }).then(KB.beautify).then(KB.string).should.eventually.equal('[{"columns":["a.name"],"data":[["A"]]}]')
   })
+
 })
-
-
-// query(
-// ['CREATE (n:Alphabet {prop}) RETURN n', 
-// {
-//  prop: [{ name: 'a', num: 1}, { name: 'b', num: 2 }]
-// }]
-// ).then(_.flow(JSON.stringify, console.log))
-
-// query(
-// [
-// ['CREATE (n:Alphabet {prop}) RETURN n', 
-// {
-//  prop: [{ name: 'a', num: 1}, { name: 'b', num: 2 }]
-// }]
-// ]
-// ).then(_.flow(JSON.stringify, console.log))
-
-// query(
-// ['CREATE (n:Alphabet {prop}) RETURN n', 
-// {
-//  prop: [{ name: 'c', num: 3}, { name: 'd', num: 4 }]
-// }],
-// ["MATCH (c),(d) WHERE c.name='c' AND d.name='d' CREATE UNIQUE (c)-[r:Next]->(d) RETURN r"]
-// ).then(_.flow(JSON.stringify, console.log))
