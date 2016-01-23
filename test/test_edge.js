a4f1d26c5db70750ab989f485253ea789bbb94fc
kengz <kengzwl@gmail.com>
2016-01-23 00:32:15 -0500

    try anon-wrap call done() for travis in addedge

--- test/test_edge.js
+++ test/test_edge.js
@@ -1,10 +1,14 @@
 suite('addEdge', function() {
   setup(function(done) {
-    A.buildGraph().then(done)
+    A.buildGraph().then(function(){
+      done()
+    })
   })
 
   teardown(function(done) {
-    A.clearTest().then(done)
+    A.clearTest().then(function(){
+      done()
+    })
   })
 
   test('(propLabelA, propLabelE, propLabelB)', function() {
@@ -16,7 +20,10 @@ suite('addEdge', function() {
   })
 
   test('([[propLabelA, propLabelE2, propLabelB], [propLabelB, propLabelE2, propLabelC]])', function() {
-    return KB.addEdge([[A.propLabelA, A.propLabelE, A.propLabelB], [A.propLabelB, A.propLabelE2, A.propLabelC]]).then(A.extractRes).then(A.string).should.eventually.equal('[{"columns":["e"],"data":[["E"]]},{"columns":["e"],"data":[["E2"]]}]')
+    return KB.addEdge([
+      [A.propLabelA, A.propLabelE, A.propLabelB],
+      [A.propLabelB, A.propLabelE2, A.propLabelC]
+    ]).then(A.extractRes).then(A.string).should.eventually.equal('[{"columns":["e"],"data":[["E"]]},{"columns":["e"],"data":[["E2"]]}]')
   })
 
 })
@@ -40,7 +47,10 @@ suite('getEdge', function() {
   })
 
   test('([[propLabelA, propLabelE, propLabelB], [propLabelD, propLabelE2, propLabelZ]])', function() {
-    return KB.getEdge([[A.propLabelA, A.propLabelE, A.propLabelB], [A.propLabelD, A.propLabelE2, A.propLabelZ]]).then(A.extractRes).then(A.string).should.eventually.equal('[{"columns":["e"],"data":[["E"]]},{"columns":["e"],"data":[["E2"]]}]')
+    return KB.getEdge([
+      [A.propLabelA, A.propLabelE, A.propLabelB],
+      [A.propLabelD, A.propLabelE2, A.propLabelZ]
+    ]).then(A.extractRes).then(A.string).should.eventually.equal('[{"columns":["e"],"data":[["E"]]},{"columns":["e"],"data":[["E2"]]}]')
   })
 
 })
